2b8aa2639c2c880780e74d4a900a3134c45fa28d
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-21 23:11:38 -0300

    Setup do cliente HTTP (ky)

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInWithEmailAndPassword } from "../sign-in/actions";
 
 export default function ForgotPasswordPage() {
   return (
-    <form action="" className="space-y-4">
+    <form action={signInWithEmailAndPassword} className="space-y-4">
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
