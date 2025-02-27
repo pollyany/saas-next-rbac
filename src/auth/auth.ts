99757508e28df1c784dcb4ba260ef6a04a542db0
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-27 17:36:18 -0300

    Adiciona suporte a permissões e modelos de autenticação com CASL

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,15 +1,49 @@
-import { getProfile } from '@/http/get-profile';
 import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'
+
+import { getMembership } from '@/http/get-membership'
+import { getProfile } from '@/http/get-profile'
+import { defineAbilityFor } from '.'
 
 export async function isAuthenticated() {
-  const cookieStore = await cookies();
-  return !!cookieStore.get('token')?.value;
+  const cookieStore = await cookies()
+  return !!cookieStore.get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  const cookieStore = await cookies()
+  return cookieStore.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token')?.value;
+  const token = (await cookies()).get('token')?.value
 
   if (!token) {
     redirect('/auth/sign-in')
---
+++ src/http/get-membership.ts
@@ -0,0 +1,22 @@
+import { Role } from '@/auth'
+import { api } from './api-client'
+
+interface GetMembershipResponse {
+  membership: {
+    id: string
+    role: Role
+    organizationId: string
+    userId: string
+  }
+}
+
+export async function getMembership(org: string) {
+  const result = await api
+    .get(`organization/${org}/membership`)
+    .json<GetMembershipResponse>()
+
+  return result
+}
+
+import { z } from 'zod'
+
