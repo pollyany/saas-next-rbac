0770bef56ec87a8b94ca54f0a37cf5f61a5d0cd3
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-28 20:57:28 -0300

    Adiciona suporte ao React Query com configuração do QueryClient e implementa o componente ProjectSwitcher para seleção de projetos

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,6 +8,7 @@ import { OrganizationSwitcher } from "./organization-switcher";
 import { ProfileButton } from "./profile-button";
 import { Separator } from "./ui/separator";
 import { ThemeSwitcher } from "./theme/theme-switcher";
+import { ProjectSwitcher } from "./project-switcher";
 
 export async function Header() {
   const permissions = await ability();
@@ -25,7 +26,12 @@ export async function Header() {
 
         <OrganizationSwitcher />
 
-        {permissions?.can("get", "Project") && <p>Project</p>}
+        {permissions?.can("get", "Project") && (
+          <>
+            <Slash className="size-3 -rotate-[24deg] text-border" />
+            <ProjectSwitcher />
+          </>
+        )}
       </div>
 
       <div className="flex items-center gap-4">
