6770772bf36d80602139b68b335a55b06a2246b1
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-28 21:32:13 -0300

    Adiciona funcionalidade para criação de projetos com integração à API e validação de dados

--- src/app/(app)/org/[slug]/create-project/actions.ts
+++ src/app/(app)/org/[slug]/create-project/actions.ts
@@ -1,53 +1,55 @@
-'use server'
+"use server";
 
-import { HTTPError } from 'ky'
-import { z } from 'zod'
+import { getCurrentOrg } from "@/auth/auth";
+import { createProject } from "@/http/create-project";
+import { HTTPError } from "ky";
+import { z } from "zod";
 
 // import { createProject } from '@/http/create-project'
 
 const projectSchema = z.object({
   name: z
     .string()
-    .min(4, { message: 'Please, incluide at least 4 characters.' }),
+    .min(4, { message: "Please, incluide at least 4 characters." }),
   description: z.string(),
-})
+});
 
 export async function createProjectAction(data: FormData) {
-  const result = projectSchema.safeParse(Object.fromEntries(data))
+  const result = projectSchema.safeParse(Object.fromEntries(data));
 
   if (!result.success) {
-    const errors = result.error.flatten().fieldErrors
+    const errors = result.error.flatten().fieldErrors;
 
-    return { success: false, message: null, errors }
+    return { success: false, message: null, errors };
   }
 
-  const { name, description } = result.data
+  const { name, description } = result.data;
 
   try {
-    // await createProject({
-    //   name,
-    //   domain,
-    //   shouldAttachUsersByDomain,
-    // })
+    await createProject({
+      org: (await getCurrentOrg()) ?? "",
+      name,
+      description,
+    });
   } catch (err) {
     if (err instanceof HTTPError) {
-      const { message } = await err.response.json()
+      const { message } = await err.response.json();
 
-      return { success: false, message, errors: null }
+      return { success: false, message, errors: null };
     }
 
-    console.error(err)
+    console.error(err);
 
     return {
       success: false,
-      message: 'Unexpected error, try again in a few minutes.',
+      message: "Unexpected error, try again in a few minutes.",
       errors: null,
-    }
+    };
   }
 
   return {
     success: true,
-    message: 'Successfully saved the project.',
+    message: "Successfully saved the project.",
     errors: null,
-  }
+  };
 }
