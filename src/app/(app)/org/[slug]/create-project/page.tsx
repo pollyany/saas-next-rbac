b13e59878bd915f98c66c587b2885ca401464e40
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-03-01 12:00:29 -0300

    Navegação do app por abas

--- src/app/(app)/org/[slug]/create-project/page.tsx
+++ src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,8 +1,8 @@
-import { Header } from '@/components/header'
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 
 import { ProjectForm } from './project-form'
-import { ability } from '@/auth/auth'
-import { redirect } from 'next/navigation'
 
 export default async function CreateProject() {
   const permissions = await ability()
@@ -10,14 +10,12 @@ export default async function CreateProject() {
   if (permissions?.cannot('create', 'Project')) {
     redirect('/')
   }
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
 
-        <ProjectForm />
-      </main>
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
+
+      <ProjectForm />
     </div>
   )
 }
